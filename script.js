2a4b378c7fd851507965550395df1e67a82520b1
SVANE11 <152732150+SVANE11@users.noreply.github.com>
2024-11-04 12:15:09 +0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Найдем элемент кнопки по ID
+
 const myButton = document.getElementById('myButton');
 
-// Добавим обработчик события клика
+
 myButton.addEventListener('click', function() {
     alert('Кнопка нажата!');
 });
